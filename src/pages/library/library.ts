42794f087b05ad6075795ed48614c88d3a4f398a
karkran <nikhil.karkra@anz.com>
2018-04-28 23:19:17 +1000

    passing quotes data in the pages and creating quotes card

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {Quote} from "../../data/quote.interface.ts"
 import quotes from "../../data/quotes.ts"
+import {QuotesPage} from "../quotes/quotes";
 @IonicPage()
 @Component({
   selector: 'page-library',
@@ -9,9 +10,12 @@ import quotes from "../../data/quotes.ts"
 })
 export class LibraryPage implements OnInit{
   quoteCollection:{category:string, quotes:Quote[], icon:string}[];
+  quotesPage=QuotesPage
+
   ngOnInit() {
     console.log(quotes)
     this.quoteCollection = quotes.data
   }
+
 
 }
--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,25 +1,18 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the QuotesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import {Component, OnInit} from '@angular/core';
+import { IonicPage, NavParams } from 'ionic-angular';
+import {Quote} from '../../data/quote.interface.ts'
 @IonicPage()
 @Component({
   selector: 'page-quotes',
   templateUrl: 'quotes.html',
 })
-export class QuotesPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class QuotesPage implements OnInit{
+  quoteGroup:{category:string, quotes:Quote[], icon:string}
+  constructor(public navParams: NavParams) {}
+  // ionViewDidLoad(){
+  //   this.quoteGroup = this.navParams.data;
+  // }
+  ngOnInit(){
+    this.quoteGroup = this.navParams.data;
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotesPage');
-  }
-
 }
