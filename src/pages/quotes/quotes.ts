8ebafc30094442a750c8222eb7227840ab556127
karkran <nikhil.karkra@anz.com>
2018-04-28 23:51:05 +1000

    fixing the card styles and favorite alert implementation

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { IonicPage, NavParams } from 'ionic-angular';
+import {IonicPage, NavParams, AlertController} from 'ionic-angular';
 import {Quote} from '../../data/quote.interface.ts'
 @IonicPage()
 @Component({
@@ -8,11 +8,38 @@ import {Quote} from '../../data/quote.interface.ts'
 })
 export class QuotesPage implements OnInit{
   quoteGroup:{category:string, quotes:Quote[], icon:string}
-  constructor(public navParams: NavParams) {}
+  constructor(
+      public navParams: NavParams,
+      private alertCtrl:AlertController
+  ) {}
   // ionViewDidLoad(){
   //   this.quoteGroup = this.navParams.data;
   // }
   ngOnInit(){
     this.quoteGroup = this.navParams.data;
+  }
+  onAddToFavorite(selectedQuote: Quote){
+   const alert = this.alertCtrl.create({
+     title:'Add Quote',
+     subtitle:'Are you sure?',
+     message:'Are you sure you want to add the quote?',
+     buttons:[
+       {
+         text:'Yes, go ahead',
+         handler:()=>{
+           console.log('ok')
+         }
+       },
+       {
+         text:'No, I changed my mind!',
+         role:'cancel',
+         handler:()=>{
+           console.log('cancelled!')
+         }
+       }
+     ]
+   });
+
+    alert.present();
   }
 }
